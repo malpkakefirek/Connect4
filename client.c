cf06ee807ccc331c639654d30a4a7830e2ed909b
malpkakefirek <malpkakefirek@gmail.com>
2024-06-17 16:04:51 +0200

    add comments

--- client.c
+++ client.c
@@ -1,20 +1,20 @@
 #include        <sys/types.h>   /* basic system data types */
 #include        <sys/socket.h>  /* basic socket definitions */
-#include        <netinet/in.h>  /* sockaddr_in{} and other Internet defns */
+#include        <netinet/in.h>  /* sockaddr_in{} and other Internet definitions */
 #include        <arpa/inet.h>   /* inet(3) functions */
-#include        <errno.h>
-#include        <stdio.h>
-#include        <stdlib.h>
-#include        <string.h>
-#include	<unistd.h>
+#include        <errno.h>       // error number definitions
+#include        <stdio.h>       // standard I/O definitions
+#include        <stdlib.h>      // standard library definitions
+#include        <string.h>      // string manipulation definitions
+#include        <unistd.h>      // POSIX standard function definitions
 
-#define MAXLINE 1024
-#define SA      struct sockaddr
+#define MAXLINE 1024           // maximum buffer size
+#define SA      struct sockaddr // alias for struct sockaddr
 
 int startClient(int argc, char **argv) {
-	int					sockfd, n;
-	struct sockaddr_in	servaddr;
-	char				recvline[MAXLINE + 1];
+	int					sockfd, n;                       // socket file descriptor and read count
+	struct sockaddr_in	servaddr;                      // server address structure
+	char				recvline[MAXLINE + 1];         // buffer for received line
 	int err;
 
 	if (argc != 2){
@@ -26,29 +26,29 @@ int startClient(int argc, char **argv) {
 		return 1;
 	}
 
-	bzero(&servaddr, sizeof(servaddr));
-	servaddr.sin_family = AF_INET;
-	servaddr.sin_port   = htons(13);	/* daytime server */
+	bzero(&servaddr, sizeof(servaddr));              // zero out the server address structure
+	servaddr.sin_family = AF_INET;                   
+	servaddr.sin_port   = htons(13);                 
 	if ( (err=inet_pton(AF_INET, argv[1], &servaddr.sin_addr)) <= 0){
-		if(err == 0 )
-			fprintf(stderr,"inet_pton error for %s \n", argv[1] );
+		if(err == 0 )                              
+			fprintf(stderr,"inet_pton error for %s \n", argv[1] ); 
 		else
-			fprintf(stderr,"inet_pton error for %s : %s \n", argv[1], strerror(errno));
+			fprintf(stderr,"inet_pton error for %s : %s \n", argv[1], strerror(errno)); 
 		return 1;
 	}
-	if (connect(sockfd, (SA *) &servaddr, sizeof(servaddr)) < 0){
-		fprintf(stderr,"connect error : %s \n", strerror(errno));
+	if (connect(sockfd, (SA *) &servaddr, sizeof(servaddr)) < 0){ // connect to the server
+		fprintf(stderr,"connect error : %s \n", strerror(errno)); 
 		return 1;
 	}
 
 	while ( (n = read(sockfd, recvline, MAXLINE)) > 0) {
 		recvline[n] = 0;	/* null terminate */
-		if (fputs(recvline, stdout) == EOF){
+		if (fputs(recvline, stdout) == EOF){        // print received data to stdout
 			fprintf(stderr,"fputs error : %s\n", strerror(errno));
 			return 1;
 		}
 	}
-	if (n < 0)
+	if (n < 0)                                     // check if read failed
 		fprintf(stderr,"read error : %s\n", strerror(errno));
 
 	fprintf(stderr,"OK\n");
--- server.c
+++ server.c
@@ -2,25 +2,25 @@
 #include        <sys/socket.h>  /* basic socket definitions */
 #include        <sys/time.h>    /* timeval{} for select() */
 #include        <time.h>        /* timespec{} for pselect() */
-#include        <netinet/in.h>  /* sockaddr_in{} and other Internet defns */
+#include        <netinet/in.h>  /* sockaddr_in{} and other Internet definitions */
 #include        <arpa/inet.h>   /* inet(3) functions */
-#include        <errno.h>
+#include        <errno.h>       // error number definitions
 #include        <fcntl.h>       /* for nonblocking */
-#include        <netdb.h>
-#include        <signal.h>
-#include        <stdio.h>
-#include        <stdlib.h>
-#include        <string.h>
-#include 	<unistd.h>
+#include        <netdb.h>       // definitions for network database operations
+#include        <signal.h>      // signal handling definitions
+#include        <stdio.h>       // standard I/O definitions
+#include        <stdlib.h>      // standard library definitions
+#include        <string.h>      // string manipulation definitions
+#include        <unistd.h>      // POSIX standard function definitions
 
-#define MAXLINE 1024
-#define LISTENQ 2
+#define MAXLINE 1024           // maximum buffer size
+#define LISTENQ 2              // maximum number of client connections
 
 int startServer(int argc, char ** argv) {
-    int listenfd, connfd;
-    socklen_t len;
-    char buff[MAXLINE], str[INET_ADDRSTRLEN + 1];
-    time_t ticks;
+    int listenfd, connfd;       
+    socklen_t len;              
+    char buff[MAXLINE], str[INET_ADDRSTRLEN + 1]; 
+    time_t ticks;               
     struct sockaddr_in servaddr, cliaddr;
 
     if ((listenfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
@@ -28,13 +28,13 @@ int startServer(int argc, char ** argv) {
         return 1;
     }
 
-    bzero( & servaddr, sizeof(servaddr));
-    servaddr.sin_family = AF_INET;
-    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-    servaddr.sin_port = htons(13); /* daytime server */
+    bzero(&servaddr, sizeof(servaddr)); // zero out the server address structure
+    servaddr.sin_family = AF_INET;      
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY); 
+    servaddr.sin_port = htons(13);      /* daytime server */
 
-    if (bind(listenfd, (struct sockaddr * ) & servaddr, sizeof(servaddr)) < 0) {
-        fprintf(stderr, "bind error : %s\n", strerror(errno));
+    if (bind(listenfd, (struct sockaddr *) &servaddr, sizeof(servaddr)) < 0) {
+        fprintf(stderr, "bind error : %s\n", strerror(errno)); 
         return 1;
     }
 
@@ -45,17 +45,17 @@ int startServer(int argc, char ** argv) {
     fprintf(stderr, "Waiting for clients ... \n");
     for ( ; ; ) {
         len = sizeof(cliaddr);
-        if ((connfd = accept(listenfd, (struct sockaddr * ) & cliaddr, & len)) < 0) {
+        if ((connfd = accept(listenfd, (struct sockaddr *) &cliaddr, &len)) < 0) {
             fprintf(stderr, "accept error : %s\n", strerror(errno));
             continue;
         }
 
-        bzero(str, sizeof(str));
-        inet_ntop(AF_INET, (struct sockaddr * ) & cliaddr.sin_addr, str, sizeof(str));
-        printf("Connection from %s\n", str);
+        bzero(str, sizeof(str)); // zero out the string buffer
+        inet_ntop(AF_INET, (struct sockaddr *) &cliaddr.sin_addr, str, sizeof(str)); // convert client address to string
+        printf("Connection from %s\n", str); // print client address
 
         ticks = time(NULL);
-        snprintf(buff, sizeof(buff), "%.24s\r\n", ctime( & ticks));
+        snprintf(buff, sizeof(buff), "%.24s\r\n", ctime(&ticks)); // format time into buffer
         if (write(connfd, buff, strlen(buff)) < 0)
             fprintf(stderr, "write error : %s\n", strerror(errno));
         close(connfd);
